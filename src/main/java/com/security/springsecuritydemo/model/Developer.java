59d2202a534459ea1a312a623fb577be125a8690
Ruslan Nurnazarov <ruslan7692@gmail.com>
2025-05-31 21:32:36 +0400

    init commit

---
+++ src/main/java/com/security/springsecuritydemo/model/Developer.java
@@ -0,0 +1,21 @@
+package com.security.springsecuritydemo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+public class Developer {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+}
